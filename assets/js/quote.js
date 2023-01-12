c52823bfc9e957ea005f7653e7339390fa3582aa
Dino Arslanovic <dino99arslanovic@hotmail.com>
2023-01-11 19:03:17 -0600

    added a quote generator with the second api

---
+++ assets/js/quote.js
@@ -0,0 +1,19 @@
+let quote = document.getElementById("quote");
+let character = document.getElementById("character");
+let anime = document.getElementById("anime")
+let btn = document.getElementById("btn");
+
+var url = "https://animechan.vercel.app/api/random";
+
+let getQuote = () => {
+    fetch(url)
+      .then((data) => data.json())
+      .then((item) => {
+        quote.innerText = item.quote;
+        character.innerText = item.character;
+        anime.innerText = item.anime
+      });
+  };
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
